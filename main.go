0476b46dacfeae2e2f05f77cef70dfc2788f91f7
Simran Tomar <87190325+lilsim@users.noreply.github.com>
2025-04-29 20:08:11 +0530

    Add files via upload

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Article struct {
+	Title   string `json:"title"`
+	Desc    string `json:"desc"`
+	Content string `json:"content"`
+}
+
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+	}
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/articles", allArticles)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
+
+func main() {
+	// Articles = []Article{
+	// 	Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+	// 	Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	// }
+	handleRequests()
+}
